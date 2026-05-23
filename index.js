cb07cca65e17458e09cbf51b80288e72f02a11eb
katherinek727 <katherinek727@outlook.com>
2026-05-23 13:13:53 +0300

    revert: restore original app for production

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import TestApp from './TestApp';
+import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => TestApp);
+AppRegistry.registerComponent(appName, () => App);
