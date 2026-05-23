51b548eaa41f5dadf84d1f22bf17ca1ea0dc97f1
katherinek727 <katherinek727@outlook.com>
2026-05-23 12:51:08 +0300

    fix: metro config for React Native 0.72.10

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,29 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
 /**
- * Metro configuration
+ * Metro configuration for React Native
  * https://reactnative.dev/docs/metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const { getDefaultConfig } = require('metro-config');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
